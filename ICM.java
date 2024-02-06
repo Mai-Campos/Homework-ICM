1e62679ff398910e56453bd82e4afca4130b084c
Maikol  Campos <157140613+Mai-Campos@users.noreply.github.com>
2024-02-06 07:36:57 -0500

    Actualizar ICM.java

--- ICM.java
+++ ICM.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ICM{
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         Scanner in = new Scanner(System.in);
         System.out.println("Ingrese su peso");
